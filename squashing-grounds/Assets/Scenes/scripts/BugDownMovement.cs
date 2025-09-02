cfefe2d614daa8cf9675d314fa38cbef90815da6
vega jafri <giantwhitedeer@gmail.com>
2025-09-02 16:03:03 -0400

    askjkfdjs

--- squashing-grounds/Assets/Scenes/scripts/BugDownMovement.cs
+++ squashing-grounds/Assets/Scenes/scripts/BugDownMovement.cs
@@ -6,6 +6,7 @@ public class BugDownMovement : MonoBehaviour
     public Transform squashPos;
     public bool isReversed = false;
     private Vector3 trajectory;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
